422eab29f5aa97c98c938e56baa137374a7f4f78
shamengxin <1921028746@qq.com>
2023-09-09 23:53:18 +0800

    模拟发送短信验证码

---
+++ yingxue-sms/src/main/java/com/shamengxin/controller/SMSController.java
@@ -0,0 +1,49 @@
+package com.shamengxin.controller;
+
+import cn.hutool.core.util.RandomUtil;
+import com.shamengxin.vo.MsgVO;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.RandomStringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+@RestController
+public class SMSController {
+
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Autowired
+    public SMSController(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
+    @PostMapping("captchas")
+    public void captchas(@RequestBody MsgVO msgVO){
+        //1. 获取手机号
+        String phone = msgVO.getPhone();
+        log.info("发送短信的手机号: {}",phone);
+
+        //2. 判断此手机号的验证码是否在有效期
+        String timeoutKey = "timeout_" + phone;
+        if (stringRedisTemplate.hasKey(timeoutKey)) {
+            throw new RuntimeException("提示: 不允许重复发送！");
+        }
+        //2. 生成验证码
+        String code = RandomStringUtils.randomNumeric(4);
+        log.info("验证码: {}",code);
+        //3. 将对应验证码放入redis中
+        String phoneKey = "phone_" + phone;
+        stringRedisTemplate.opsForValue().set(phoneKey,code,10, TimeUnit.MINUTES);
+
+        //4.如果验证码在有效期内，不允许重新发送
+        stringRedisTemplate.opsForValue().set(timeoutKey,"true",15,TimeUnit.MINUTES);
+    }
+
+}
---
+++ yingxue-sms/src/main/java/com/shamengxin/vo/MsgVO.java
@@ -0,0 +1,14 @@
+package com.shamengxin.vo;
+
+public class MsgVO {
+
+    private String phone;
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
